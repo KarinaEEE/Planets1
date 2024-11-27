5de017f95d2ca40215e4a802e63d27441771b048
opilane <opilane@HP-CZC0359NNX>
2024-11-27 12:13:43 +0200

    Add project files.

---
+++ MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiPlanet
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    
+                    fonts.AddFont("dogicabold.otf", "RegularFront");
+                    fonts.AddFont("VCR_OSD_MONO_1.001.ttf", "MediumFrontd");
+                    fonts.AddFont("dogicapixel.otf", "BoldFront");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
